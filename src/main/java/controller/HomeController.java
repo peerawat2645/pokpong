1485d80bd680dde5e98f6e539d7269d5761c3123
vaok2004 <peerawat.won@ku.th>
2022-09-22 15:28:57 +0700

    Update HomeController.java

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -9,9 +9,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
+	@Autowired
+	private PersonService personService ;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+		System.out.println("Home Page Requested, locale = " + locale);
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 
+		String formattedDate = dateFormat.format(date);
+
+		
+		model.addAttribute("serverTime", formattedDate);
+
+		model.addAttribute("persons",  personService.findAll());
 		return "home";
 	}
 }
